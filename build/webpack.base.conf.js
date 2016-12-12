22fc7545ab50b7f7bc884ca7457909582ea09951
feizheng <1290657123@qq.com>
2016-12-12 22:44:23 +0800

    minify package

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -10,8 +10,9 @@ var cssSourceMapDev = (env === 'development' && config.dev.cssSourceMap)
 var cssSourceMapProd = (env === 'production' && config.build.productionSourceMap)
 var useCssSourceMap = cssSourceMapDev || cssSourceMapProd;
 var externals = process.env.NODE_ENV === 'production' ? {
-  react: 'React',
-  'react-dom': 'ReactDOM'
+  react: 'react',
+  classnames: 'classnames',
+  'react-dom': 'react-dom'
 } : {};
 
 module.exports = {
@@ -36,8 +37,8 @@ module.exports = {
   },
   plugins:[
     new webpack.ProvidePlugin({
-        'React': 'react',
-        'ReactDOM': 'react-dom'
+      'React': 'react',
+      'ReactDOM': 'react-dom'
     }),
   ],
   resolveLoader: {
