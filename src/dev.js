cfd88b8ae1647edd22f1c594af42251c849c03d7
afeiship <1290657123@qq.com>
2017-03-23 10:22:31 +0800

    update demo

--- src/dev.js
+++ src/dev.js
@@ -1,5 +1,5 @@
 import {Indicator,IndicatorCtrl} from './main';
-
+import './dev.scss';
 
 class App extends React.Component{
   componentWillMount(){
@@ -16,17 +16,25 @@ class App extends React.Component{
   }
 
   _click2(){
-    IndicatorCtrl.show({text:'测试中...',backdropStyle:{opacity:0.01}});
+    IndicatorCtrl.show({text:'backdrop0.01...',backdropStyle:{opacity:0.01}});
     setTimeout(function(){
       IndicatorCtrl.hide();
     },2000)
   }
 
+  _click3(){
+    IndicatorCtrl.show({text:'no backdrop...',backdropStyle:{opacity:0.01}});
+    setTimeout(function(){
+      IndicatorCtrl.hide();
+    },1000)
+  }
+
   render(){
     return (
       <div className="hello-indicator">
-        <button onClick={this._click1.bind(this)}>show indicator1</button>
-        <button onClick={this._click2.bind(this)}>show indicator2</button>
+        <button onClick={this._click1.bind(this)}>show indicator1-normal</button>
+        <button onClick={this._click2.bind(this)}>show indicator2-（backdropStyle:0.01）</button>
+        <button onClick={this._click3.bind(this)}>show indicator3-（backdrop:false）</button>
       </div>
     );
   }
